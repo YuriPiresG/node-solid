cef1aab5932051c561c9b7208ea226a9d9f5c05f
Yuri Pires <yuripiresbr@gmail.com>
2023-10-25 17:41:40 -0300

    feat: added new fields to CheckIn, such as Long and Lat

--- src/services/checkin.service.ts
+++ src/services/checkin.service.ts
@@ -1,9 +1,13 @@
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
 import { CheckIn } from "@prisma/client";
+import { ResourceNotFound } from "./errors/resource-not-found-error";
 
 interface CheckInServiceRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 type CheckInServiceResponse = {
@@ -11,12 +15,29 @@ type CheckInServiceResponse = {
 };
 
 export class CheckInService {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) throw new ResourceNotFound();
+
+    //calculate distance between user and gym
+    //if distance is greater than 100m, throw error
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+
+    if (checkInOnSameDate) throw new Error();
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
--- src/services/checkin.spec.ts
+++ src/services/checkin.spec.ts
@@ -1,20 +1,81 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-checkins-repository";
-import { expect, describe, it, beforeEach } from "vitest";
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { CheckInService } from "./checkin.service";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository copy";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInService;
 
 describe("Check-in Use Case", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInService(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      description: "Academia 1",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+      phone: "123456789",
+      title: "Academia 1",
+    });
+
+    sut = new CheckInService(checkInsRepository, gymsRepository);
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to check in", async () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check in twice a day", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+        userLatitude: 0,
+        userLongitude: 0,
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should be able to check in twice in different days", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
