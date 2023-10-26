267b68cb729737279425f646b21ba4836c7b1630
Yuri Pires <yuripiresbr@gmail.com>
2023-10-26 14:21:28 -0300

    refactor: Typed the In memory data

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,19 +1,29 @@
 import { Gym, Prisma } from "@prisma/client";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
 import { randomUUID } from "crypto";
 import { Decimal } from "@prisma/client/runtime/library";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
+interface GymData {
+  id?: string;
+  title: string;
+  description?: string;
+  phone?: string;
+  latitude: Decimal;
+  longitude: Decimal;
+  created_at?: Date;
+}
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
 
-  async create(data: Prisma.GymCreateInput) {
+  async create(data: GymData) {
     const gym = {
       id: data.id ?? randomUUID(),
       title: data.title,
       description: data.description ?? null,
       phone: data.phone ?? null,
-      latitude: new Decimal(data.latitude.toString()),
-      longitude: new Decimal(data.longitude.toString()),
+      latitude: new Decimal(data.latitude.toString() ?? "0"),
+      longitude: new Decimal(data.longitude.toString() ?? "0"),
       created_at: new Date(),
     };
 
@@ -36,5 +46,21 @@ export class InMemoryGymsRepository implements GymsRepository {
     return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20);
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        {
+          latitude: params.latitude,
+          longitude: params.longitude,
+        },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+      return distance < 10;
+    });
   }
 }
--- src/services/search-gyms.spec.ts
+++ src/services/search-gyms.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
 import { expect, describe, it, beforeEach } from "vitest";
 import { SearchGymsService } from "./search-gyms.service";
-
+import { Decimal } from "@prisma/client/runtime/library";
 
 let gymsRepository: InMemoryGymsRepository;
 let sut: SearchGymsService;
@@ -14,19 +14,21 @@ describe("Search Gyms Use Case", () => {
 
   it("should be able to search for gyms", async () => {
     await gymsRepository.create({
+      id: "gym-01",
+      description: "Academia 01",
+      latitude: new Decimal(-22.5772356),
+      longitude: new Decimal(-47.4196327),
+      phone: "123456789",
       title: "Academia 01",
-      description: null,
-      phone: null,
-      latitude: -27.2092052,
-      longitude: -49.6401091,
     });
 
     await gymsRepository.create({
+      id: "gym-01",
+      description: "Academia 02",
+      latitude: new Decimal(-22.5772356),
+      longitude: new Decimal(-47.4196327),
+      phone: "123456789",
       title: "Academia 02",
-      description: null,
-      phone: null,
-      latitude: -27.2092052,
-      longitude: -49.6401091,
     });
 
     const { gyms } = await sut.execute({
@@ -35,31 +37,30 @@ describe("Search Gyms Use Case", () => {
     });
 
     expect(gyms).toHaveLength(1);
-    expect(gyms).toEqual([
-      expect.objectContaining({ title: "JavaScript Gym" }),
-    ]);
+    expect(gyms).toEqual([expect.objectContaining({ title: "Academia 01" })]);
   });
 
-  it.skip("should be able to fetch paginated gym search", async () => {
+  it("should be able to fetch paginated gym search", async () => {
     for (let i = 1; i <= 22; i++) {
       await gymsRepository.create({
-        title: `JavaScript Gym ${i}`,
-        description: null,
-        phone: null,
-        latitude: -27.2092052,
-        longitude: -49.6401091,
+        id: "gym-01",
+        description: `Gym ${i}`,
+        latitude: new Decimal(-22.5772356),
+        longitude: new Decimal(-47.4196327),
+        phone: "123456789",
+        title: `Gym ${i}`,
       });
     }
 
     const { gyms } = await sut.execute({
-      query: "JavaScript",
+      query: "Gym",
       page: 2,
     });
 
     expect(gyms).toHaveLength(2);
     expect(gyms).toEqual([
-      expect.objectContaining({ title: "JavaScript Gym 21" }),
-      expect.objectContaining({ title: "JavaScript Gym 22" }),
+      expect.objectContaining({ title: "Gym 21" }),
+      expect.objectContaining({ title: "Gym 22" }),
     ]);
   });
 });
